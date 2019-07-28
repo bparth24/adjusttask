f9f8aed1d5248e886cb9e04c9a9d9db424db1987
Parth Bhatt <parth@cvlending.com>
2019-07-28 16:23:26 -0700

    Updates for adjust task

--- mysite/settings.py
+++ mysite/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'adjust_app',
 ]
 
 MIDDLEWARE = [
@@ -78,6 +79,16 @@ DATABASES = {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
+    # },
+    #
+    # 'default1': {
+    #     'ENGINE': 'django.db.backends.postgresql_psycopg2',
+    #     "NAME": "adjust",
+    #     "USER": "postgres",
+    #     "PASSWORD": "postgres",
+    #     "HOST": "localhost",
+    #     "PORT": "5432",
+    # },
 }
 
 
